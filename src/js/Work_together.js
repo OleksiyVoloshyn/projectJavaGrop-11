963faadd15f03b8e6fe368e80c46dcf6b791e781
OleksiyGromov <gromov196312@gmail.com>
2024-12-22 22:48:53 +0200

    POST

--- src/js/Work_together.js
+++ src/js/Work_together.js
@@ -3,13 +3,32 @@ import '../css/styles.css';
 
 
 
-const BASE_URL = "https://portfolio-js.b.goit.study/api-docs/#/Requests/post_requests";
+const BASE_URL = "https://portfolio-js.b.goit.study/api";
+const REQUEST_URL = ""
 const form = document.querySelector('.form-subscribe');
 const buttonSend = document.querySelector('.footer-button-form');
 
-form.addEventListener("submit", getInfo);
-function getInfo(event) {
-    event.preventDefault();
-    const { email, comment } = event.target.elements;
-    console.log(email.value, comment.value);
+function fetchData(url = BASE_URL, options = {}) {
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+    })
 }
+buttonSend.addEventListener('click', getClick);
+
+function getClick(event) {
+    const { email, comment } = event.target.elements;
+    fetchData(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email: email.value, comment: comment.value })
+    })
+        .then(data => console.log(data))
+        .catch(error => console.log(error.message))
+} 
+
