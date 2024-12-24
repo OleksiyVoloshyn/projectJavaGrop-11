24180670cc8e228199221b118367a5a5c167ddb9
Mariianath <doma1122v@gmail.com>
2024-12-24 15:46:29 +0100

    fixe items

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,25 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const accordion = new Accordion('.accordion-container', {
-        duration: 300, 
-        showMultiple: false, 
+        duration: 300,
+        showMultiple: false,
         elementClass: 'ac',
         triggerClass: 'ac-trigger',
         panelClass: 'ac-panel',
         activeClass: 'is-active',
         onOpen: (element) => {
             const panel = element.querySelector('.ac-panel');
+            const icon = element.querySelector('.icon use');
             if (panel) {
-                
                 panel.style.maxHeight = panel.scrollHeight + "px";
                 panel.style.overflow = "visible";
+            }
+            if (icon) {
+                icon.setAttribute('xlink:href', '#icon-arrowUP');
             }
         },
         onClose: (element) => {
             const panel = element.querySelector('.ac-panel');
+            const icon = element.querySelector('.icon use');
             if (panel) {
-                
                 panel.style.maxHeight = null;
                 panel.style.overflow = "hidden";
+            }
+            if (icon) {
+                icon.setAttribute('xlink:href', '#icon-arrowDOWN');
             }
         }
     });
